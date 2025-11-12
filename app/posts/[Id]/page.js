9b2723ed61319c13ca32e45e110956b1b4e40a9a
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-11-12 09:17:24 +0100

    Add PostDetail component for displaying individual post details

--- app/posts/[Id]/page.js
+++ app/posts/[Id]/page.js
@@ -1,17 +1,17 @@
 import styles from "../../page.module.css";
 
 export default async function PostDetail({ params }) {
-  const { postId } = await params;
-  console.log("post id:", postId);
+  const { Id } = await params;
+  console.log("post id:", Id);
 
-  const url = `https://fb-rest-race-default-rtdb.firebaseio.com/posts/${postId}.json`;
+  const url = `https://fb-rest-race-default-rtdb.firebaseio.com/posts/${Id}.json`;
   const response = await fetch(url);
   const post = await response.json();
   console.log("pst: ", post);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Post detail page for page: {postId}</h1>
+        <h1>Post detail page for page: {post.caption}</h1>
       </main>
     </div>
   );
